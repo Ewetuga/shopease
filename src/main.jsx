102038a6ec21badd74304ac55080190862dd6587
Ewetuga <109183013+Ewetuga@users.noreply.github.com>
2025-06-12 11:19:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App.jsx';         // Go up one level
-import '../App.css';                  // Go up one level
-import '../index.css';                // Go up one level
-import '../styles.css';              // Go up one level
+import App from './App.jsx';
+import './App.css';
+import './index.css';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
